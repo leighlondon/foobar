4b5bbba8cd9c92e6a6eff50652e1860d20d38515
Leigh London <leigh.00.london@gmail.com>
2014-12-03 13:29:25 +1100

    pirate: solution.
    The submitted solution to the "pirate walks into a bar" problem.

--- a_pirate_walks_into_a_bar/solution.py
+++ a_pirate_walks_into_a_bar/solution.py
@@ -1,2 +1,22 @@
 def answers(numbers):
-    # code.
+
+    # Create an empty list, to put the pirates as they're met.
+    pirates = []
+    # Start at pirate 0.
+    i = 0
+
+    # Keep going until the cycle is found.
+    while True:
+
+        # Check the current pirate matches any existing pirates.
+        # As they're added sequentially, any previous pirate means that
+        # there's now a cycle. The length of the cycle is the length of
+        # the list, minus the index of the pirate that was just found.
+        if i in pirates:
+            return len(pirates) - pirates.index(i)
+
+        # Otherwise, append the current pirate.
+        pirates.append(i)
+
+        # Move to the next pirate.
+        i = numbers[i]
